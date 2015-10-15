9057b9ba9256f529093d45274236456e429d5580
Lucas Weissert <Weissert40@gmail.com>
2015-10-15 20:17:31 +0200

    modify crawler

--- Lucas_Weissert/crawler.py
+++ Lucas_Weissert/crawler.py
@@ -33,32 +33,42 @@ r = requests.get(URL)
 soup = BeautifulSoup(r.text, 'html.parser')
 #print soup
 
+def informationRecommandation(text):
+	informations = text.findAll(['a', 'li'])
+	return informations[0].text.encode('utf-8').split(' - '), informations[1].text.encode('utf-8'), informations[2].text.encode('utf-8'), informations[3].text.encode('utf-8')
+
+
 for node in soup.findAll("div", {"class":"yt-lockup-content"}):
-		informations = node.findAll(['a', 'li'])
-		print informations[0].text.encode('utf-8').split(' - ')
-		print informations[1].text.encode('utf-8')
-		print informations[2].text.encode('utf-8')
-		print informations[3].text.encode('utf-8')
+
+		print informationRecommandation(node)
+		infs = informationRecommandation(node)
+		print type(infs)
+		print infs[0]
+		# informations = node.findAll(['a', 'li'])
+		# print informations[0].text.encode('utf-8').split(' - ')
+		# print informations[1].text.encode('utf-8')
+		# print informations[2].text.encode('utf-8')
+		# print informations[3].text.encode('utf-8')
 		
 
-		chanteur_titre = informations[0].text.encode('utf-8').split(' - ')
-		if len(chanteur_titre) < 2:
-			continue
-		print ('taille ' + str(len(chanteur_titre)))
-		chanteurs.append(chanteur_titre[0])
-		titres.append(chanteur_titre[1])
-		comptes_youtube.append(informations[1].text.encode('utf-8'))
-		vues.append(informations[2].text.encode('utf-8'))
-		dates.append(informations[3].text.encode('utf-8'))
+		# chanteur_titre = informations[0].text.encode('utf-8').split(' - ')
+		# if len(chanteur_titre) < 2:
+		# 	continue
+		# print ('taille ' + str(len(chanteur_titre)))
+		# chanteurs.append(chanteur_titre[0])
+		# titres.append(chanteur_titre[1])
+		# comptes_youtube.append(informations[1].text.encode('utf-8'))
+		# vues.append(informations[2].text.encode('utf-8'))
+		# dates.append(informations[3].text.encode('utf-8'))
 
 		# for informations in node.findAll(['a', 'li']):
 		# 	print informations.text.encode('utf-8')
 		print '_________'
 
-print chanteurs
-print Counter(chanteurs)
+# print chanteurs
+# print Counter(chanteurs)
 
-df = pandas.DataFrame.from_dict(Counter(chanteurs), orient='index')
-df.plot(kind='bar')
-plt.show()
+# df = pandas.DataFrame.from_dict(Counter(chanteurs), orient='index')
+# df.plot(kind='bar')
+# plt.show()
 # df.plot(title='Title Here')
