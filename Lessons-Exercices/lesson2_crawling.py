6247ffe5f4a706bccc5ee089722f4679abfca7e3
Charles Miglietti <charles.miglietti@yahoo.fr>
2015-10-15 21:26:00 +0200

    refacto like/dislike

--- Lessons-Exercices/lesson2_crawling.py
+++ Lessons-Exercices/lesson2_crawling.py
@@ -8,6 +8,12 @@ from bs4 import BeautifulSoup
 def extractIntFromText(text):
   return int(text.replace(u'\xa0', u''))
 
+def extractGenericLike(classname):
+  like_button = soup.findAll("button", { "class" : classname })[0]
+  like_count_str = like_button.findAll("span", { "class" : "yt-uix-button-content" })[0].text
+  like_count = extractIntFromText(like_count_str)
+  return like_count
+
 #Execute q request toward Youtube
 request = requests.get('https://www.youtube.com/watch?v=e82VE8UtW8A')
 #parse the restult of the request
@@ -17,15 +23,9 @@ soup = BeautifulSoup(request.text, 'html.parser')
 view_count_str = soup.findAll("div", { "class" : "watch-view-count" })[0].text
 view_count = extractIntFromText(view_count_str)
 
-
 #GEt the like count
-like_button = soup.findAll("button", { "class" : "like-button-renderer-like-button" })[0]
-like_count_str = like_button.findAll("span", { "class" : "yt-uix-button-content" })[0].text
-like_count = extractIntFromText(like_count_str)
-
+like_count = extractGenericLike("like-button-renderer-like-button")
 #GEt the like count
-dislike_button = soup.findAll("button", { "class" : "like-button-renderer-dislike-button" })[0]
-dislike_count_str = dislike_button.findAll("span", { "class" : "yt-uix-button-content" })[0].text
-dislike_count = extractIntFromText(dislike_count_str)
+dislike_count = extractGenericLike("like-button-renderer-dislike-button")
 
 print 'Hello'
