8eea186a2e6fc3031b6ab61e3d47b35e0b018aa6
Charles Miglietti <charles.miglietti@yahoo.fr>
2015-10-15 21:25:00 +0200

    get like count

--- Lessons-Exercices/lesson2_crawling.py
+++ Lessons-Exercices/lesson2_crawling.py
@@ -10,9 +10,15 @@ from bs4 import BeautifulSoup
 request = requests.get('https://www.youtube.com/watch?v=e82VE8UtW8A')
 #parse the restult of the request
 soup = BeautifulSoup(request.text, 'html.parser')
+
+#GEt the view count
 view_count_str = soup.findAll("div", { "class" : "watch-view-count" })[0].text
 view_count = int(view_count_str.replace(u'\xa0', u''))
 
 
+#GEt the like count
+like_button = soup.findAll("button", { "class" : "like-button-renderer-like-button" })[0]
+like_count_str = like_button.findAll("span", { "class" : "yt-uix-button-content" })[0].text
+like_count = int(like_count_str.replace(u'\xa0', u''))
 
 print 'Hello'
