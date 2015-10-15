1b9e1c9619b8331307bcdf888f4004b18b3e44d0
Charles Miglietti <charles.miglietti@yahoo.fr>
2015-10-15 21:24:45 +0200

    get view count

--- Lessons-Exercices/lesson2_crawling.py
+++ Lessons-Exercices/lesson2_crawling.py
@@ -1,3 +1,5 @@
+# coding: utf8
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,6 +10,8 @@ from bs4 import BeautifulSoup
 request = requests.get('https://www.youtube.com/watch?v=e82VE8UtW8A')
 #parse the restult of the request
 soup = BeautifulSoup(request.text, 'html.parser')
+view_count_str = soup.findAll("div", { "class" : "watch-view-count" })[0].text
+view_count = int(view_count_str.replace(u'\xa0', u''))
 
 
 
