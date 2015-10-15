ae412d1cdc9ec361ca3780214ea42a2ccb0bc0d5
Charles Miglietti <charles.miglietti@yahoo.fr>
2015-10-15 21:26:50 +0200

    refacto extractMetaFromUrl

--- Lessons-Exercices/lesson2_crawling.py
+++ Lessons-Exercices/lesson2_crawling.py
@@ -8,33 +8,41 @@ from bs4 import BeautifulSoup
 def extractIntFromText(text):
   return int(text.replace(u'\xa0', u''))
 
-def extractGenericLike(classname):
+def extractGenericLike(soup,classname):
   like_button = soup.findAll("button", { "class" : classname })[0]
   like_count_str = like_button.findAll("span", { "class" : "yt-uix-button-content" })[0].text
   like_count = extractIntFromText(like_count_str)
   return like_count
 
-#Execute q request toward Youtube
-request = requests.get('https://www.youtube.com/watch?v=e82VE8UtW8A')
-#parse the restult of the request
-soup = BeautifulSoup(request.text, 'html.parser')
+def extractMetricsFromUrl(url):
+  #Execute q request toward Youtube
+  request = requests.get(url)
+  #parse the restult of the request
+  soup = BeautifulSoup(request.text, 'html.parser')
 
-#GEt the view count
-view_count_str = soup.findAll("div", { "class" : "watch-view-count" })[0].text
-view_count = extractIntFromText(view_count_str)
+  #GEt the view count
+  view_count_str = soup.findAll("div", { "class" : "watch-view-count" })[0].text
+  view_count = extractIntFromText(view_count_str)
 
-#GEt the like count
-like_count = extractGenericLike("like-button-renderer-like-button")
-#GEt the like count
-dislike_count = extractGenericLike("like-button-renderer-dislike-button")
+  #GEt the like count
+  like_count = extractGenericLike(soup,"like-button-renderer-like-button")
+  #GEt the like count
+  dislike_count = extractGenericLike(soup,"like-button-renderer-dislike-button")
 
-metrics = {}
-metrics['view_count'] = view_count
-metrics['like_count'] = like_count
-metrics['dislike_count'] = dislike_count
-metrics['indicator'] = 1000.* (like_count - dislike_count) / view_count
+  metrics = {}
+  metrics['view_count'] = view_count
+  metrics['like_count'] = like_count
+  metrics['dislike_count'] = dislike_count
+  metrics['indicator'] = 1000.* (like_count - dislike_count) / view_count
 
-print '===='
-print 'Handling ' , soup.title.text
-print 'The like count is', like_count, ' and dislike ', dislike_count
-print 'Popularity indicator is ' , metrics['indicator']
+  print '===='
+  print 'Handling ' , soup.title.text
+  print 'The like count is', like_count, ' and dislike ', dislike_count
+  print 'Popularity indicator is ' , metrics['indicator']
+  print '===='
+  return metrics
+
+
+extractMetricsFromUrl('https://www.youtube.com/watch?v=B3eAMGXFw1o')
+extractMetricsFromUrl('https://www.youtube.com/watch?v=Ao8cGLIMtvg')
+extractMetricsFromUrl('https://www.youtube.com/watch?v=lWA2pjMjpBs')
