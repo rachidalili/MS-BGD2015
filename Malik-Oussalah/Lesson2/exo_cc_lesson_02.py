6a5b74aed14fcb1591f96171cc4169a258e53b61
Oussalah <malik.oussalah@telecom-paristech.fr>
2015-10-20 10:27:43 +0200

    MAJ crawling cdiscount

--- Malik-Oussalah/Lesson2/exo_cc_lesson_02.py
+++ Malik-Oussalah/Lesson2/exo_cc_lesson_02.py
@@ -1,24 +1,24 @@
-#Crawling sur la page Cdiscount récupération lien/nomproduit/prixprod/#
+#Crawling sur la page Cdiscount récupération lien/nomproduit/prixprod#
 import requests
 from bs4 import BeautifulSoup
 
 
 def getSoupFromUrl(url):
-    #Execute q request toward Youtube
+    #Execute q request toward url
     request = requests.get(url)
-    #parse the restult of the request
+    #parse the result of the request
     soup = BeautifulSoup(request.text, 'html.parser')
     return soup
 
-def extractPostFromPage(url):
+def GetInfosFromPage(url):
     soup = getSoupFromUrl(url)
     # body = soup.find_all('div', {'class': 'content'})
     blocs = soup.find_all('div', {'class': 'prdtBloc'})
     for bloc in blocs:
-        title = bloc.find('div',{'class':'prdtBTit'}).text.encode('utf-8')
+        title = bloc.find('div',{'class':'prdtBTit'}).text
         html = bloc.find('a').get('href')
-        descr = bloc.find('p', {'class': 'prdtBDesc'}).text.encode('utf-8')
-        price = bloc.find('span',{'class':'price'}).text.encode('utf-8')
+        descr = bloc.find('p', {'class': 'prdtBDesc'}).text
+        price = bloc.find('span',{'class':'price'}).text
         print "lien : ", html
         print " "
         print "nom : ", title
@@ -33,10 +33,11 @@ def extractPostFromPage(url):
 
 
 def start():
-    choix = input("Entrer le produit que vous voulez scrapper!")
+    choix = input("Entrer votre recherche : ")
     choix = str(choix)
     url = "http://www.cdiscount.com/search/10/{}.html".format(choix.replace(" ","+"))
     extractPostFromPage(url)
 
+ #Launching Crawling#
 
 start()
