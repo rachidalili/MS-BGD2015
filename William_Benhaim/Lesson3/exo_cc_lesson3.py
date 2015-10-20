423c5738478832337c07659f1ecaeb45f5bbd198
williamben <william.benhaim@gmail.com>
2015-10-20 12:55:33 +0200

    CC

--- William_Benhaim/Lesson3/exo_cc_lesson3.py
+++ William_Benhaim/Lesson3/exo_cc_lesson3.py
@@ -12,16 +12,14 @@ def FromTextToTextWithPlus(txt):
 #for year in range(firstYear,LastYear+1):
 def GetInfo(NameProduct):
 	url='http://www.cdiscount.com/search/10/'+NameProduct+'.html#_his_'
-	print(url)
+	#print(url)
 	soup = GetSoupFromUrl(url)
 	tile = soup.findAll("div", { "class" : "prdtBloc" })
 	for tiles_links in tile:
 		title=tiles_links.findAll("div",{"class":"prdtBTit"})[0].text
 		print 'title ',title
-		# oldPrice=tiles_links.find("div",{"class":"prdtPrSt"})[0].text
-		# if oldPrice is not None and len(oldPrice)>0:
-		# 	oldPrice=tiles_links.findAll("div",{"class":"prdtPrSt"})[0].text
-		# 	print 'oldPrice ',oldPrice
+		for oldPrice in tiles_links.findAll("div",{"class":"prdtPrSt"}):
+			print 'oldPrice ',oldPrice.text.encode('utf-8')
 		newPrice=tiles_links.findAll("span",{"class":"price"})[0].text
 		print 'newPrice ',newPrice
 		Des=tiles_links.findAll("p",{"class":"prdtBDesc"})[0].text
@@ -29,5 +27,5 @@ def GetInfo(NameProduct):
 
 NameProduct='acer aspire'
 NameInForm=FromTextToTextWithPlus(NameProduct)
-print NameInForm
+#print NameInForm
 GetInfo(NameInForm)
