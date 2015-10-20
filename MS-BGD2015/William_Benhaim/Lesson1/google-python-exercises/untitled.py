2d883260062da70f44a4312533dfe64c3d943862
FlorianFirmin <florian.firmin@telecom-paristech.fr>
2015-10-20 20:22:27 +0200

    suppression du sous-dossier MS créé par erreur

--- MS-BGD2015/William_Benhaim/Lesson1/google-python-exercises/untitled.py
+++
@@ -1,9 +0,0 @@
-for i in range[0:100]:
-    test=" "
-    if i%3==0:
-        test=test+'fiz'
-    if i%5==0:
-        test=test+'buzz'
-    if test[-1]!='z':
-        test=test+i
-print test
--- MS-BGD2015/mas/README.md
+++
@@ -1,1 +0,0 @@
-# files of M. Amine SALEM
