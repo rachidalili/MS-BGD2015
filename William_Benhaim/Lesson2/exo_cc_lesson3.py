c85aa29db95c739ccc3f8cb069d68ee29cf1a0f4
williamben <william.benhaim@gmail.com>
2015-10-20 09:35:30 +0200

    new CC

--- William_Benhaim/Lesson2/exo_cc_lesson3.py
+++ William_Benhaim/Lesson2/exo_cc_lesson3.py
@@ -6,20 +6,28 @@ def GetSoupFromUrl(url):
 	return BeautifulSoup(request.text, 'html.parser')
 def FromTestToInt(txt):
 	return int(txt.replace(u'\u20ac',u' ').replace(' ',''))
-
+def FromTextToTextWithPlus(txt):
+	return txt.replace(' ','+')
 
 #for year in range(firstYear,LastYear+1):
-url='http://www.cdiscount.com/search/10/acer+aspire.html#_his_'
-soup = GetSoupFromUrl(url)
-tile = soup.findAll("div", { "class" : "prdtBloc" })
-for tiles_links in tile:
-	title=tiles_links.findAll("div",{"class":"prdtBTit"})[0].text
-	print 'title ',title
-	# oldPrice=tiles_links.find("div",{"class":"prdtPrSt"})[0].text
-	# if oldPrice is not None and len(oldPrice)>0:
-	# 	oldPrice=tiles_links.findAll("div",{"class":"prdtPrSt"})[0].text
-	# 	print 'oldPrice ',oldPrice
-	newPrice=tiles_links.findAll("span",{"class":"price"})[0].text
-	print 'newPrice ',newPrice
-	Des=tiles_links.findAll("p",{"class":"prdtBDesc"})[0].text
-	print 'Des ',Des
+def GetInfo(NameProduct):
+	url='http://www.cdiscount.com/search/10/acer+aspire.html#_his_'
+	print(url)
+	soup = GetSoupFromUrl(url)
+	tile = soup.findAll("div", { "class" : "prdtBloc" })
+	for tiles_links in tile:
+		title=tiles_links.findAll("div",{"class":"prdtBTit"})[0].text
+		print 'title ',title
+		# oldPrice=tiles_links.find("div",{"class":"prdtPrSt"})[0].text
+		# if oldPrice is not None and len(oldPrice)>0:
+		# 	oldPrice=tiles_links.findAll("div",{"class":"prdtPrSt"})[0].text
+		# 	print 'oldPrice ',oldPrice
+		newPrice=tiles_links.findAll("span",{"class":"price"})[0].text
+		print 'newPrice ',newPrice
+		Des=tiles_links.findAll("p",{"class":"prdtBDesc"})[0].text
+		print 'Des ',Des
+
+NameProduct='acer aspire'
+NameInForm=FromTextToTextWithPlus(NameProduct)
+print NameInForm
+GetInfo(NameInForm)
