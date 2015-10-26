999c39dd2a9bc741994c700050fb9b77b8fa7630
Oussalah <malik.oussalah@telecom-paristech.fr>
2015-10-26 21:35:19 +0100

    MAJ exo_dom_lesson_03.py

--- Malik-Oussalah/Lesson3/exo_dom_lesson_03.py
+++ Malik-Oussalah/Lesson3/exo_dom_lesson_03.py
@@ -4,68 +4,77 @@
 # Course INFMDI 721 - Lesson 3
 # Author : Malik OUSSALAH
 
-
+#Package Importation############
 
 import requests
 from bs4 import BeautifulSoup
-from numpy import mean
-import pandas as pd
-import urllib2
+import urllib2, base64
+import operator
 import json
 
-
-def authentification():
-    """
-    Since this script requires more than 60 API request / hours,
-    we need to authentificate as a GitHub user.
-    Returns:
-    --------
-    tuple (username, password)
-    """
-    username = str(input('GitHub username:'))
-    password = str(input('GitHub password:'))
-    r = requests.get('https://api.github.com/user', auth=(username, password))
-    if r.status_code != 200:
-        print('Incorrect username/password, please retry')
-        return authentification()
-    return (username, password)
+################################
 
 
-def GetContributors(auth):
-    """list of the most active users """
+
+#Configuration####################################################
+
+GIT_LOGIN = 'MalikOussalah'
+# Get your token there : https://github.com/settings/tokens/new
+AUTH_KEY = '9442e5ba8bd8ef216367008f9c9341500d59fc99'
+
+##################################################################
+
+#Function needed###############################################################
+
+def start(auth):
+    """function to get all repo users sorted by mean of stars"""
     users = {}
     r = requests.get(url='https://gist.github.com/paulmillr/2657075/',
             auth=auth)
     soup = BeautifulSoup(r.text,'html.parser')
     print " "
-    print "Username " + "    " + " Contribution"
-    print "#############################################"
+    print "Username " + "    " + " Contribution" + "       " + " Mean_Stars"
+    print "###################################################################"
     print " "
+    starsMean = {}
     for tr in soup('tbody')[0].select('tr'):
         user_name = tr.select('td')[0].text
         user_contrib = tr.select('td')[1].text
         users[user_name] = user_contrib
+        user_repo = tr.select('td')[0].text.split(" ")[0]
+        user_json = GetJson(user_repo)
+        if user_json:
+            allrepos = json.loads(user_json)
+            sumstars = 0.0
+            for el in allrepos:
+                sumstars += el['stargazers_count']
+            starsMean[user_repo] = sumstars / len(allrepos)
         #print user_name + " " + user_contrib
-        print user_name, user_contrib
+        print user_name, user_contrib, starsMean[user_repo]
+        sortedMean = sorted(starsMean.items(), key=operator.itemgetter(1), reverse=True)
+    print sortedMean
 
-def GetJson(user):
-    GIT_API_REPOS = "https://api.github.com/users/%user%/repos"
-    req = urllib2.Request(url=GIT_API_REPOS.replace('%user%',user))
-    response = urllib2.urlopen(req)
-    return response.read().strip()
+def GetJson (user):
+    """function that get you the Json format of a user repo"""
+    URL_GIT_API_REPOS_BY_USER = 'https://api.github.com/users/%USER%/repos'
+    request = urllib2.Request(URL_GIT_API_REPOS_BY_USER.replace('%USER%', user))
+    base64string = base64.encodestring('%s:%s' % (GIT_LOGIN, AUTH_KEY)).replace('\n', '')
+    request.add_header("Authorization", "Basic %s" % base64string)
+    try:
+        response = urllib2.urlopen(request)
+        return response.read().strip()
+    except urllib2.HTTPError, err:
+        if err.code == 404:
+            print str(user) + " : repos not found"
+        else:
+            print "Error " + str(err.code) + " : " + str(err.reason)
 
+###############################################################################
 
-#TEST
-r = requests.get(url='https://gist.github.com/paulmillr/2657075/',
-            auth=("MalikOussalah","VX6yLCbv"))
-soup = BeautifulSoup(r.text, 'html.parser')
-#for tr in soup('tbody')[0].select('tr'):
-#    print tr.select('td')[0].text
-#prendre que le premier element
-print soup('tbody')[0].select('td')[0].text.split(" ")[0]
+#Main###############################
 
-#GetContributors(authentification())
+start(("MalikOussalah","VX6yLCbv"))
 
-
+####################################
           
 
