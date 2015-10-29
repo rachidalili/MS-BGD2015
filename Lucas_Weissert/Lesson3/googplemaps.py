964fc746c0e031646fc32b47320cd2429e3a49e9
Lucas Weissert <Weissert40@gmail.com>
2015-10-29 16:09:18 +0100

    google api

--- Lucas_Weissert/Lesson3/googplemaps.py
+++ Lucas_Weissert/Lesson3/googplemaps.py
@@ -21,10 +21,6 @@ data_df = pandas.read_csv('/Users/lucasweissert/MS-BGD2015/Lessons-Exercices/vil
 print data_df
 
 villes = data_df['Ville']
-# Departement = data_df['Departement']
-
-url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&language=fr-FR&key=AIzaSyDLdf1PFF0DpZaRJjHUm52HPAmWH9ZxwIk'
-print url
 
 dests = villes[0:10]
 for ville in villes[0:10]:
@@ -33,4 +29,7 @@ for ville in villes[0:10]:
 		url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + ville + '&destinations=' + dest +'&mode=bicycling&language=fr-FR&key=AIzaSyDLdf1PFF0DpZaRJjHUm52HPAmWH9ZxwIk'
 		googleResponse = urllib.urlopen(url)
 		jsonResponse = json.loads(googleResponse.read())
-		print ville + ' to ' + dest + ' : ' + removeSpecialCarFromText(jsonResponse['rows'][0]['elements'][0]['distance']['text'])
+		row = jsonResponse['rows']
+		elements = row[0]['elements']
+		distance = removeSpecialCarFromText(elements[0]['distance']['text'])
+		print ville + ' to ' + dest + ' : ' + distance
