5b10e98d6ba96d9d0ebb9e9bb31fcb9b269ef83e
rachid-alili <rachid.alili@free.fr>
2015-10-30 13:31:59 +0100

    maj

---
+++ rachid-alili/lesson4/tp4.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import csv
+
+cr1 = csv.reader(open("villes.csv", ",") )
+cr2 = csv.reader(open("villes.csv", ",") )
+
+for row1 in cr1:
+	for row2 in cr2:
+        print row1[0] 
+        print row2[0] 
+        if row1[0] != row2[0]:
+      	 # appeler le web_services
+      	print ws.methode (row1[0] , row2[0] )
+
+       
+
+
+# demarche
+je regarde google
+le plus dur est le doule 
+regarder le double map
+
+regarder les dataframes
+
+# utiliser distance matric 
+# utiliser client ggogle map utilser le client python googles maps services python 
+# regarder developpeur console 
+# il y a un identi
+ #api key
+ utiliser le google client pip install -u 
+ copier l'exemple 
+
+
+la suite mode = transit : pur le train, etc.
+lambda fonction (paremetre d'entree )
+
+
+regarder les api
+pygithub
+github 3 
+regarder urllib 
+request : orenté métier : plus propre.
+
