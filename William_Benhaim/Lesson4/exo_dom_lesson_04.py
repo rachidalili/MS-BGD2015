38b843bdc613dd8828fb41ddd13822a829794c45
williamben <william.benhaim@gmail.com>
2015-11-01 22:05:14 +0100

    Dom 4 with phone

--- William_Benhaim/Lesson4/exo_dom_lesson_04.py
+++ William_Benhaim/Lesson4/exo_dom_lesson_04.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 def FromTestToInt(txt):
    	return int(txt.replace(u'\xa0', u'').replace(u'\u20ac', u'').replace(' ',''))
@@ -10,26 +11,15 @@ def GetSoupFromUrl(url):
     request = requests.get(url)
     return BeautifulSoup(request.text, 'html.parser')
 
-
-def GetInformation(soup):
-    like = GetButton(soup, "like-button-renderer-like-button")
-    print 'like:', like
-    dislike = GetButton(soup, "like-button-renderer-dislike-button")
-    print 'dislike:', dislike
-    nbview = soup.findAll("div", {"class": "watch-view-count"})[0].text
-    intnbview = FromTestToInt(nbview)
-    print 'NbView:', intnbview
-    print('#####')
-
 def recupKM(txt):
 	return int(txt.replace(u'KM', u'').replace(u'\nKilom\xe9trage :', u'').replace(' ',''))
 
 def typeversion(Alldes):
-    if 'ZEN' or 'Zen' or 'zen' in Alldes.text:
+    if 'ZEN' in Alldes.text or 'Zen' in Alldes.text or 'zen' in Alldes.text:
     	return 'ZEN'
-    if 'Life' or 'LIFE' or 'life'in Alldes.text:
+    if  'Life' in Alldes.text or 'LIFE' in Alldes.text or 'life' in Alldes.text:
     	return 'LIFE'
-    if 'Intens' or 'INTENS' or 'intens'in Alldes.text:
+    if 'INTENS' in Alldes.text or 'Intens' in Alldes.text or 'intens' in Alldes.text:
     	return 'INTENS'
 
 
@@ -40,12 +30,6 @@ def coteLacentrale(modele,annee):
 	return FromTestToInt(Cote.text)
 
 
-
-
-def GetSoupFromUrl(url):
-	request=requests.get(url)
-	return BeautifulSoup(request.text, 'html.parser')
-	
 def GetInformation(url_zoe,str_name_region):
 	listinfo=[]
 	soupInfo = GetSoupFromUrl(url_zoe)
@@ -70,7 +54,7 @@ def GetInformation(url_zoe,str_name_region):
 	listinfo.append(km)
 	prix = FromTestToInt(soupInfo.find('span', {'class': 'price'}).text)
 	listinfo.append(prix)
-	if annee in range(2012,2015):
+	if annee in range(2012,2015) and version is not None:
 		cote=(coteLacentrale(version,str(annee)))
 		if cote>prix:
 			CompareCoteToprice='low'
@@ -79,9 +63,18 @@ def GetInformation(url_zoe,str_name_region):
 	else:
 		cote='NA'
 		CompareCoteToprice='NA'
-	
+	expression = r"0[0-9]([ .-/]?[0-9]{2}){4}"
+	#phone=re.search(expression,des)
 
-	return str_name_region,version,annee,typevendeur,km,prix,cote,CompareCoteToprice
+	isphone= re.search(expression,str(des))
+	if isphone:
+		phone=isphone.group()
+	else:
+		phone='NA'
+	return str_name_region,version,annee,typevendeur,phone,km,prix,cote,CompareCoteToprice
+
+def isZoe(libelle):
+	return "Zo" in libelle or "zo" in libelle or "ZO" in libelle
 
 
 def LeBonCoinZoe(listregion):
@@ -96,17 +89,17 @@ def LeBonCoinZoe(listregion):
 		cars = bloc_all_cars.findAll('a')
 		for car in cars:
 			
-			if ('Renault') in car['title']:
+			if isZoe(car['title']):
 				url_zoe=car['href']
 				listcar.append(GetInformation(url_zoe,str_name_region))
 
 	return listcar
 
-listInfo=['Region','Version','Annee','Type de Vendeur','KM','Prix','Cote','Comparaisan Prix vs Cote']
-listregion=['ile_de_france','aquitaine']
+listInfo=['Region','Version','Annee','Type de Vendeur','Telephone','KM','Prix','Cote','Comparaisan Prix vs Cote']
+listregion=['ile_de_france','aquitaine','provence_alpes_cote_d_azur']
 listALL=LeBonCoinZoe(listregion)
 
 Result_DF = pd.DataFrame(listALL, columns=listInfo)
-
+print Result_DF
 
 
