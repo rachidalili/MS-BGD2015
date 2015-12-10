73ddb3aac03513df91c39966de85b092b25de37b
Derrick HO <derrickho92@gmail.com>
2015-12-10 14:40:58 +0100

    maj

--- Derrick_HO/Lesson3/crawlerGitHub.py
+++ Derrick_HO/Lesson3/crawlerGitHub.py
@@ -1,73 +1,47 @@
-# coding: utf8
-
-import requests
-import json
-from bs4 import BeautifulSoup
-import urllib2
 import pandas as pd
-import operator
+import requests
+import re
+import json
+
 
 URL = 'https://gist.github.com/paulmillr/2657075'
-# GIT_LOGIN ############ Complete
-# GIT_PWD ############ Complete
-# GIT_TOKEN 
+# TOKEN_GIT TO DO
 
-def getSoupFromUrl(url):
-	request = requests.get(url)
-	soup = BeautifulSoup(request.text, 'html.parser')
-	return soup
+def request():
+    r = requests.get(URL)
+    text = r.text.encode('utf-8')
+    return text
 
-def getUsers(url):
-	soup = getSoupFromUrl(url)
-	usersList = []
+def regextext(text):
+    p = re.compile('<td><a href="https://github.com/(.*?)">')
+    m = p.findall(text)
+    return m
 
-	blocs = soup.find_all('article', {'class': 'markdown-body entry-content'})
-	table = soup.find('table')
-	rows = table.find_all('tr')
-	for row in rows:
-		cell = row.find_all('td')	
-		if cell:
-			href = cell[0].find_all('a', href=True)
-			name = href[0].text
-			# link = href[0]['href']
-			# usersList[name] =  link
-			usersList.append(name)
-	return usersList
-
-# Authentification github par id/mdp
-def requestGithubAPI(user):
-	request = requests.get('https://api.github.com/users/' + user + '/repos', auth=(GIT_LOGIN, GIT_PWD))
-	return request
-
-# Authentification gitHub par Token
-def authGihubAPI(user):
+def requestAPI(user):
 	url = 'https://api.github.com/users/' + user + '/repos'
-	headers = {'Authorization': 'token %s' % GIT_TOKEN}
+	headers = {'Authorization': 'token %s' % TOKEN_GIT}
 	r = requests.get(url, headers=headers)
-	return r
+	return r.text.encode('utf-8')
 
-def getMean(jsonText):
-	sum = 0
-	for attributes in jsonText:
-		sum+=attributes['stargazers_count']
-	mean = float(sum)/ float(len(jsonText))
+def getMean(jsonRepos):
+	total = 0.0
+    	for repo in jsonRepos:
+    		total += repo['stargazers_count']
+    	mean = total / len(jsonRepos)
 	return mean
 
 def main():
-	users = getUsers(URL)
-	usersDict = {}
-	for name in users:
-		# r = requestGithubAPI(name)
-		r = authGihubAPI(name)
-		jsonText = json.loads(r.text)
-		mean = getMean(jsonText)
+    text = request()
+    users = regextext(text)
+    meanDict = {}
+    for user in users:
+    	repos = requestAPI(user)
+    	jsonRepos = json.loads(repos)
+    	mean = getMean(jsonRepos)
+    	meanDict[user] = mean
 
-		if name not in usersDict:
-			usersDict[name] = mean
-			
-	usersDictSorted = sorted(usersDict.items(), key=operator.itemgetter(1))
-	pdDict = pd.DataFrame(usersDictSorted)
-	print pdDict
+    for k,v in meanDict.iteritems():
+    	print k +': ' + str(v)
 
 if __name__ == '__main__':
-	main()
+    main()
